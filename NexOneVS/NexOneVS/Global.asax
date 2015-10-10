c5749f4a889a0db5036322936d6bef6f367259b2
Nate McDorman <ntmcdorman@gmail.com>
2015-10-10 11:33:21 -0700

    Initial VS MVC Push
    This is the initial project push for the team to setup the files and
    what not.

---
+++ NexOneVS/NexOneVS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NexOneVS.MvcApplication" Language="C#" %>
---
+++ NexOneVS/NexOneVS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NexOneVS.Startup))]
+namespace NexOneVS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
